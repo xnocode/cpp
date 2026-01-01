2cc5af222efa8bf122e7bf5c1a8facbcd867a80e
Ridoy <xridoynocode@gmail.com>
2026-01-01 18:09:50 +0600

    first commit

---
+++ 07. Variables & Data Types/02_macros.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#define PI 3.1416
+using namespace std;
+int main()
+{
+    cout << PI << endl;
+}
---
+++ 07. Variables & Data Types/04_variables.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = 5;
+    b = 50;
+    int c;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+}
