298cd4261769f23abf7472bac154d6958bf68021
Ridoy <xridoynocode@gmail.com>
2026-01-01 18:40:19 +0600

    Size Of Data Types
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

---
+++ 07. Variables & Data Types/05_size_of_data_types.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int age = 25;
+    cout << "size of int = " << sizeof(int) << endl;
+    char grade = 'A';
+    cout << "size of char = " << sizeof(char) << endl;
+    bool Isadult = true;
+    cout << Isadult << endl;
+    cout << "size of bool = " << sizeof(bool) << endl;
+}
