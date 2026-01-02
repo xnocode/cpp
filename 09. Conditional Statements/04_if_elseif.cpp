4a3686c90a840bd8200826c88fc9084b12cd45a5
Ridoy <xridoynocode@gmail.com>
2026-01-02 23:00:30 +0600

    Add program to demonstrate if-else conditional statements
    Signed-off-by: Ridoy <xridoynocode@gmail.com>

---
+++ 09. Conditional Statements/04_if_elseif.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cin >> x;
+    if (x >= 90)
+    {
+        cout << "A" << endl;
+    }
+    else if (x >= 80)
+    {
+        cout << "B" << endl;
+    }
+    else
+    {
+        cout << "C" << endl;
+    }
+}
